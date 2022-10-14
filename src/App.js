2d1d7dbbdada1c4a8809bccb70ba82ed3c6810ae
shivansh-jsx <shivansh@dataclog.com>
2022-10-14 17:02:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const map_url = `https://api.openweathermap.org/data/2.5/onecall?`;
+  const city_url = `https://api.openweathermap.org/data/2.5/weather?`;
+  const API_KEY = `cda84759274791123030464c248efe8f`;
+  const [lat, setLat]  = useState(null); 
+  const [long, setLong]  = useState(null);
+  const [cityName, setCityName] = useState("");
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    let apiCall = `${map_url}lat=${lat}&lon=${long}&exclude=current,minutely&units=metric&appid=${API_KEY}`;
+    let cityApiCall = `${city_url}lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`;
+
+    axios.get(cityApiCall)
+    .then((response) => {
+      console.log('weather : ', response.data);
+      setCityName(response.data.name);
+    })
+    axios.get(apiCall)
+    .then((response) => {
+      console.log('onecall : ', response.data);
+    })
+  }, [lat, long]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <p>Your coordinates are - </p>
+     <p>Lat : {lat}</p>
+     <p>Long : {long}</p>
+     <p>City : {cityName}</p>
+   </div>
   );
 }
 
